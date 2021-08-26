6b33a11c8ab321808a175764d2ffa32956528bb9
ppavankumar727 <ppavankumar727>
2021-08-26 20:17:49 +0530

    dummy front end done

--- frontend/src/components/courselist/courselist.js
+++ frontend/src/components/courselist/courselist.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Course } from '../course/course';
 
 export class Courselist extends React.Component{
     render(){
         return (
-
+          <div className="album py-5 bg-light">
             <div className="container">
             <div className="row">
-              <div className="col-md-3"><img className="flex-grow-0" src="assets/img/1778502_f4b9_12.jpg" width="150 rem" /></div>
-              <div className="col-md-3"><img className="flex-grow-0" src="assets/img/1778502_f4b9_12.jpg" width="150 rem" /></div>
-              <div className="col"><img className="flex-grow-0" src="assets/img/1778502_f4b9_12.jpg" width="150 rem" /></div>
-              <div className="col-md-3"><img className="flex-grow-0" src="assets/img/1778502_f4b9_12.jpg" width="150 rem" /></div>
+                <Course>
+                </Course>
+                <Course>
+                </Course>
+                <Course>
+                </Course>
+                <Course>
+                </Course>
             </div>
+          </div>
           </div>
           );
       
