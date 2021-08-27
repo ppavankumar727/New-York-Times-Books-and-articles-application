ea4f54d99b658797f8e719980ca79669c2029605
ppavankumar727 <ppavankumar727>
2021-08-28 04:45:26 +0530

    api fetch complete

--- frontend/src/components/course/course.js
+++ frontend/src/components/course/course.js
@@ -4,8 +4,8 @@ export class Course extends React.Component{
     render(){
         return (
               <div className="col-md-3"><img className="flex-grow-0" src="assets/img/1778502_f4b9_12.jpg" width="150 rem" />
-                <h4>{this.props.name}</h4>
-                <h5>Rating : Author :</h5>
+                <h4>{this.props.title}</h4>
+                <h5>Author :{this.props.author}</h5>
               </div>
           );
       
--- frontend/src/components/courselist/courselist.js
+++ frontend/src/components/courselist/courselist.js
@@ -4,31 +4,21 @@ import { Course } from '../course/course';
 export class Courselist extends React.Component{
   constructor(props){
     super(props);
-    this.state = {courseName:""};
-    this.search =  this.search.bind(this);
-    this.search();
   }
-  search(){
-    return fetch('/api/').then(res =>{
-      if(res.ok){
-        return res.json();
-      }
-    }).then(res=>{
-      console.log(res);
-      this.setState({courseName:res.hello})
-    })
-  }
+
     render(){
         return (
           <div className="album py-5 bg-light">
             <div className="container">
+            <div className="row" >
+               <h1 className="col-6">Top books</h1>
+               <input className="form-control-sm ms-auto" type="search" />
+            </div>
             <div className="row">
-                <Course name = {this.state.courseName}>
-                </Course>
-                <Course name = {this.state.courseName}>
-                </Course>
-                <Course name = {this.state.courseName}>
-                </Course>
+                {this.props.booklist && this.props.booklist.map(course => {
+                  return <Course title={course.book_details[0].title} author={course.book_details[0].author}>
+                  </Course>
+                })} 
             </div>
           </div>
           </div>
