269d0f9eebfc454ca4e59f327b09d9fe87884702
ppavankumar727 <ppavankumar727>
2021-08-27 12:56:23 +0530

    Set Up Front And Backend

--- frontend/src/components/course/course.js
+++ frontend/src/components/course/course.js
@@ -4,7 +4,7 @@ export class Course extends React.Component{
     render(){
         return (
               <div className="col-md-3"><img className="flex-grow-0" src="assets/img/1778502_f4b9_12.jpg" width="150 rem" />
-                <h4>Course Name</h4>
+                <h4>{this.props.name}</h4>
                 <h5>Rating : Author :</h5>
               </div>
           );
--- frontend/src/components/courselist/courselist.js
+++ frontend/src/components/courselist/courselist.js
@@ -2,18 +2,32 @@ import React from 'react';
 import { Course } from '../course/course';
 
 export class Courselist extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {courseName:""};
+    this.search =  this.search.bind(this);
+    this.search();
+  }
+  search(){
+    return fetch('/api/').then(res =>{
+      if(res.ok){
+        return res.json();
+      }
+    }).then(res=>{
+      console.log(res);
+      this.setState({courseName:res.hello})
+    })
+  }
     render(){
         return (
           <div className="album py-5 bg-light">
             <div className="container">
             <div className="row">
-                <Course>
+                <Course name = {this.state.courseName}>
                 </Course>
-                <Course>
+                <Course name = {this.state.courseName}>
                 </Course>
-                <Course>
-                </Course>
-                <Course>
+                <Course name = {this.state.courseName}>
                 </Course>
             </div>
           </div>
